1cf8b7811368cb3d654ea94e3e8c250f3d2a8178
shubham <shubham.hatkar9765@gmail.com>
2023-04-07 17:37:08 +0530

    getters and setters

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public void setName(String value)
+    {
+        this.name = value;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
 }
